ee68afb99ff6909654daaafcd0ab61171d9a7925
krish <krish@ENGRSTU16_36>
2017-03-18 21:09:27 -0500

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Location of Python
+            string PythonExecutable = @"C:\Python\python.exe";
+
+            //Location of Python File
+            string PythonFileToCall = "C:\\Sample\\Sample.py";
+            
+            //Arguments to be passed
+            int x = 20 , y = 3;
+
+
+            //Process Start Information for the Python Process
+            ProcessStartInfo PythonProcessStartInfo = new ProcessStartInfo(PythonExecutable);
+
+
+            //If we need to access the standard output, set this to false
+            PythonProcessStartInfo.UseShellExecute = false;
+
+            PythonProcessStartInfo.RedirectStandardOutput = true;
+
+            //Arguments
+            PythonProcessStartInfo.Arguments = PythonFileToCall + " " + x + " " + y;
+
+            //The main process
+            Process PythonProcess = new Process();
+
+            //Process information
+            PythonProcess.StartInfo = PythonProcessStartInfo;
+
+
+            Console.WriteLine("Calling Python script with arguments {0} and {1}", x, y);
+
+            //Start The process
+            PythonProcess.Start();
+
+            
+            //Get the output of the python process
+            StreamReader StreamReader = PythonProcess.StandardOutput;
+
+            //Read to the end, the output
+            string PythonOutput = StreamReader.ReadToEnd();
+
+            PythonProcess.WaitForExit();
+            PythonProcess.Close();
+
+            Console.WriteLine(PythonOutput);
+
+        }
+    }
+}
